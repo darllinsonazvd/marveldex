e322c13a886f3ac49304086f352901ab3f9ea184
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2022-06-22 09:12:56 -0300

    fixing request in api

--- app/public/character-details-container/characterDetailsContainerController.js
+++ app/public/character-details-container/characterDetailsContainerController.js
@@ -25,31 +25,31 @@ function CharacterDetailsController(charactersService, $stateParams) {
     vm.characterName = vm.characterData.name;
     vm.characterDescription = vm.characterData.description;
     vm.characterPhoto = vm.characterData.thumbnail.path;
-
-    vm.characterComicsData = charactersService
-      .seeComics(vm.characterId, vm.offset, vm.limit)
-      .then(response => {
-        vm.comics = response.data.data.results;
-      });
-
-    vm.characterSeriesData = charactersService
-      .seeSeries(vm.characterId, vm.offset, vm.limit)
-      .then(response => {
-        vm.series = response.data.data.results;
-      });
-
-    vm.characterStoriesData = charactersService
-      .seeStories(vm.characterId, vm.offset, vm.limit)
-      .then(response => {
-        vm.stories = response.data.data.results;
-      });
-
-    vm.characterEventsData = charactersService
-      .seeEvents(vm.characterId, vm.offset, vm.limit)
-      .then(response => {
-        vm.events = response.data.data.results;
-      });
   });
+
+  vm.characterComicsData = charactersService
+    .seeComics(vm.characterId, vm.offset, vm.limit)
+    .then(response => {
+      vm.comics = response.data.data.results;
+    });
+
+  vm.characterSeriesData = charactersService
+    .seeSeries(vm.characterId, vm.offset, vm.limit)
+    .then(response => {
+      vm.series = response.data.data.results;
+    });
+
+  vm.characterStoriesData = charactersService
+    .seeStories(vm.characterId, vm.offset, vm.limit)
+    .then(response => {
+      vm.stories = response.data.data.results;
+    });
+
+  vm.characterEventsData = charactersService
+    .seeEvents(vm.characterId, vm.offset, vm.limit)
+    .then(response => {
+      vm.events = response.data.data.results;
+    });
 
   vm.seeMoreComics = () => {
     vm.offset += vm.limit;
